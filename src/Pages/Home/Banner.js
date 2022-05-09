5f321a40b975592794df82de74193244261dd8e0
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-05-10 02:33:13 +0600

    Added info section

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -3,7 +3,7 @@ import chair from '../../assets/images/chair.png';
 
 const Banner = () => {
   return (
-    <div class="hero min-h-screen">
+    <div class="hero min-h-screen px-12">
       <div class="hero-content flex-col lg:flex-row-reverse">
         <img
           src={chair}
@@ -11,13 +11,15 @@ const Banner = () => {
           alt="a surgical chair"
         />
         <div>
-          <h1 class="text-5xl font-bold">Box Office News!</h1>
+          <h1 class="text-5xl font-bold">Your New Smile Starts Here</h1>
           <p class="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
           </p>
-          <button class="btn btn-primary">Get Started</button>
+          <button class="btn btn-primary text-white font-bold bg-gradient-to-r from-secondary to-primary">
+            Get Started
+          </button>
         </div>
       </div>
     </div>
