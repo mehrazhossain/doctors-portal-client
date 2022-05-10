3d2db874eb8942f03e24edddf626d049c728ce25
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-05-11 01:09:50 +0600

    added AppointmentBanner component & calender

---
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+import chair from '../../assets/images/chair.png';
+
+const AppointmentBanner = () => {
+  const [date, setDate] = useState(new Date());
+  return (
+    <div class="hero min-h-screen">
+      <div class="hero-content flex-col lg:flex-row-reverse">
+        <img
+          src={chair}
+          class="max-w-sm rounded-lg shadow-2xl"
+          alt="Dentist chair"
+        />
+        <div>
+          <DayPicker mode="single" selected={date} onSelect={setDate} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AppointmentBanner;
