b1b4c9e11604f0dc9fe50817387c5489efef105a
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-05-12 10:50:58 +0600

    added createUserWithEmailAndPassword

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import {
+  useSignInWithGoogle,
+  useCreateUserWithEmailAndPassword,
+} from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
+import Loading from '../Shared/Loading';
 
 const Login = () => {
-  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+  let loadingSpinner;
 
   const {
     register,
@@ -14,9 +21,23 @@ const Login = () => {
 
   const onSubmit = (data) => {
     console.log(data);
+    createUserWithEmailAndPassword(data.email, data.password);
   };
 
-  if (user) {
+  if (loading || gLoading) {
+    return <Loading></Loading>;
+  }
+  let signInError;
+  if (error || gError) {
+    signInError = (
+      <p className="mb-2 font-semibold">
+        <small className="text-red-500">
+          {error?.message || gError?.message}
+        </small>
+      </p>
+    );
+  }
+  if (gUser) {
     console.log(user);
   }
   return (
@@ -89,6 +110,7 @@ const Login = () => {
                 )}
               </label>
             </div>
+            {signInError}
             <input
               className="btn w-full max-w-xs "
               type="submit"
