5aafbe75b3403028002a3fde509b07ddea9f16be
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-05-15 10:55:12 +0600

    Load available time slots based on the date

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -7,11 +7,15 @@ const AvailableAppointments = ({ date }) => {
   const [services, setServices] = useState([]);
   const [treatment, setTreatment] = useState(null);
 
+  const formattedDate = format(date, 'PP');
+
   useEffect(() => {
-    fetch('http://localhost:5000/service')
+    fetch(`http://localhost:5000/available?date=${formattedDate}`)
       .then((res) => res.json())
       .then((data) => setServices(data));
+    console.log(formattedDate);
   }, []);
+
   return (
     <div>
       <h4 className="text-xl text-secondary text-center font-semibold my-12">
