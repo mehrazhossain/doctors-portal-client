abcd1bb243e7099dcbefae8be21a968c091d701e
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-05-19 01:06:30 +0600

    Set email after appointment confirmation & Delete confirmation modal

--- src/Pages/Dashboard/DoctorRow.js
+++ src/Pages/Dashboard/DoctorRow.js
@@ -1,26 +1,9 @@
 import React from 'react';
 import { toast } from 'react-toastify';
 
-const DoctorRow = ({ doctor, index, refetch }) => {
+const DoctorRow = ({ doctor, index, refetch, setDeletingDoctor }) => {
   const { name, specialty, img, email } = doctor;
 
-  const handleDelete = (email) => {
-    fetch(`http://localhost:5000/doctor/${email}`, {
-      method: 'DELETE',
-      headers: {
-        'content-type': 'application/json',
-        authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        if (data.deletedCount) {
-          toast.success(`Doctor: ${name} is deleted.`);
-          refetch();
-        }
-      });
-  };
   return (
     <tr>
       <th>{index}</th>
@@ -34,12 +17,13 @@ const DoctorRow = ({ doctor, index, refetch }) => {
       <td>{name}</td>
       <td>{specialty}</td>
       <td>
-        <button
-          onClick={() => handleDelete(email)}
+        <label
+          onClick={() => setDeletingDoctor(doctor)}
+          for="delete-confirmed-modal"
           class="btn btn-xs btn-error"
         >
           Delete
-        </button>
+        </label>
       </td>
     </tr>
   );
